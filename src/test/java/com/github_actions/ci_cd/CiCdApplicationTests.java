eda155fd1c82c9a056af5f944ec10a129cb3bf1e
Griotold <gotjd9773@naver.com>
2024-11-27 20:26:51 +0900

    run-tes.yml 삭제

--- src/test/java/com/github_actions/ci_cd/CiCdApplicationTests.java
+++ src/test/java/com/github_actions/ci_cd/CiCdApplicationTests.java
@@ -1,6 +1,5 @@
 package com.github_actions.ci_cd;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +8,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CiCdApplicationTests {
